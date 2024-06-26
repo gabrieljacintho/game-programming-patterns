055027a50e03c9f964cff7faa5b8791ad95e3cd8
gabrieljacintho <gabrieljacintho868@gmail.com>
2024-06-26 15:28:08 -0400

    Singletons and Update Manager scripts

---
+++ ApplicationManager.cs
@@ -0,0 +1,23 @@
+﻿// Credits: Gabriel Bertasso - contact@gabrielbertasso.com
+
+using UnityEngine;
+
+namespace Patterns
+{
+    public static class ApplicationManager
+    {
+        public static bool IsQuitting { get; private set; }
+
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        private static void Initialize()
+        {
+            Application.quitting += OnQuitting;
+        }
+        
+        private static void OnQuitting()
+        {
+            IsQuitting = true;
+        }
+    }
+}
---
+++ PersistentSingleton.cs
@@ -0,0 +1,23 @@
+// Credits: Gabriel Bertasso - contact@gabrielbertasso.com
+
+using UnityEngine;
+
+namespace Patterns
+{
+    public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+    {
+        protected override void Awake()
+        {
+            if (s_instance == null)
+            {
+                s_instance = this as T;
+                transform.parent = null;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (s_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
